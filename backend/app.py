9f8e029506de087228ad7931a15bf59d2597d69d
Shiva-sai-cmd <kodudhalashivasai@gmail.com>
2025-08-04 20:21:14 +0530

    Fix: Use pymysql instead of MySQLdb

--- backend/app.py
+++ backend/app.py
@@ -1,25 +1,33 @@
+import sys
+import os
+import pymysql
+
+class FakeCommand:
+    COM_QUERY = 3
+
+sys.modules['MySQLdb.constants.COMMAND'] = FakeCommand
+
+pymysql.install_as_MySQLdb()
+
 from flask import Flask
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager
 from dotenv import load_dotenv
-import pymysql
-import os
 
 from config import Config
 from extensions import mysql, mail
 
-pymysql.install_as_MySQLdb()
 load_dotenv()
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
 CORS(app)
+
 mysql.init_app(app)
 mail.init_app(app)
 jwt = JWTManager(app)
 
-# Register Blueprints
 from routes.auth_routes import auth_bp
 from routes.todo_routes import todo_bp
 
